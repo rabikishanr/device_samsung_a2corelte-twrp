fc48f321a8f23fc41f5256a3aad190b2366c02dc
Rabikishan Rauniyar <154669639+rabikishanr@users.noreply.github.com>
2024-07-03 16:03:40 +0545

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,8 +44,8 @@ BOARD_CACHEIMAGE_PARTITION_SIZE    := 0x00C800000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/samsung/a2corelte/Image
-TARGET_PREBUILT_DTB := device/samsung/a2corelte/dtb.img
-TARGET_PREBUILT_DT := device/samsung/a2corelte/dt.img
+TARGET_PREBUILT_DTB := device/samsung/a2corelte/dt.img
+
 
 # Use this flag if the board has a ext4 partition larger than 2gb
 BOARD_HAS_LARGE_FILESYSTEM := true
